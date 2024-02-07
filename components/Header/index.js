2516d7476dee5325aab3ff5e142939e70d50b316
Rawan Aladdin <raladdin@se14l0120-07.ad.bcit.ca>
2024-02-07 15:24:37 -0800

    finishing components

--- components/Header/index.js
+++ components/Header/index.js
@@ -2,12 +2,17 @@ import styles from './Header.module.css'
 
 export default function Header() {
     return(
-        <header className={styles.header}>
-            <h1>Aladdin Family Vacation</h1>
-            <p>
-                Enter a Bitmask Number into the input section to bring 
-                an Aladdin family member on the family vacation. 
-            </p>
-        </header>
+        <div className={styles.container}>
+            <header className={styles.header}>
+                <h1>Aladdin Family Vacation</h1>
+                <p>
+                    Enter a Bitmask Number into the input section to bring 
+                    an Aladdin family member on the family vacation. <br />
+                    Add the numbers together to take different members together 
+                    or even all of them.
+                    Different combinations bring different members!
+                </p>
+            </header>
+        </div>
     )
 }
